7f68bdf806d4b5a13ec515bbfc60a68580f563aa
HugoM1711 <116550014+HugoM1711@users.noreply.github.com>
2023-03-22 15:34:36 +0100

    php

---
+++ connexion.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "stagexplorer";
+
+// Créer la connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+  die("Erreur de connexion: " . $conn->connect_error);
+}
+echo "Connecté avec succès";
+?>
