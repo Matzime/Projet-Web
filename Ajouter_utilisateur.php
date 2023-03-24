e48a1f3d7df7bc34461eaf8307c2d287d23b9613
HugoM1711 <116550014+HugoM1711@users.noreply.github.com>
2023-03-24 10:26:21 +0100

    dfgh

--- Ajouter_utilisateur.php
+++ Ajouter_utilisateur.php
@@ -8,15 +8,21 @@ $nom = $_POST['nom'];
 $mail = $_POST['mail'];
 $password = $_POST['password'];
 $passwordVerif = $_POST['passwordVerif'];
+$role = $_POST['role'];
 
 // Chiffrer le mot de passe (s'il a bien été répété deux fois)
     if($password==$passwordVerif){
 $motdepasse_chiffre = password_hash($password, PASSWORD_DEFAULT);}
 
+// Déterminer l'ID du role choisie (pour préparer jointure table utilisateur-role)
+if($role=="Pilote"){$ID_role=2;}
+else{$ID_role=3;}
+
+
 // Préparer la requête SQL pour ajouter l'utilisateur
-$sql = "INSERT INTO utilisateur (prenom, nom, mail, motdepasse_chiffre) VALUES (?, ?, ?)";
+$sql = "INSERT INTO utilisateur (Prenom, Nom, Mail, MDP, ID_Role) VALUES (?, ?, ?, ?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $nom, $email, $motdepasse_chiffre);
+$stmt->bind_param('ssssi', $prenom, $nom, $mail, $motdepasse_chiffre, $ID_role);
 
 // Exécuter la requête
 if ($stmt->execute()) {
