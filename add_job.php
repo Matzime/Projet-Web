69e6b89c9d0fca2e1e22c0c716e1807f62730120
DavidRmb <david.rmb44@gmail.com>
2023-03-27 17:24:39 +0200

    job

--- add_job.php
+++ add_job.php
@@ -2,7 +2,7 @@
 require_once 'data_base_connexion.php';
 
 // Récupérer les données du formulaire d'inscription
-$entreprise = $_POST['enterprise'];
+$enterprise = $_POST['enterprise'];
 $salary = $_POST['salary'];
 $period = $_POST['period'];
 $seats = $_POST['seats'];
@@ -12,6 +12,9 @@ $skill = $_POST['skill'];
 //Récupération, décomposition et validation de l'adresse
 $address = urlencode($_POST['address']); // Encodage de l'adresse pour la requête
 $apiKey = 'f7506853d19a4b5e9fa3382af20257f8'; 
+
+//Récupération date aujourd'hui
+$datePublication = date("Y-m-d");
 
 // Effectuer une requête HTTP vers l'API Geocoding
 $url = "https://api.opencagedata.com/geocode/v1/json?q={$address}&key={$apiKey}";
@@ -25,7 +28,6 @@ $responseData = json_decode($response, true);
 
 if (isset($responseData['results'][0])) {
     $addressDetails = $responseData['results'][0]['components'];
-
     $road = $addressDetails['road'] ?? '';
     $number = $addressDetails['house_number'] ?? '';
     $town = $addressDetails['town'] ?? $addressDetails['city'] ?? '';
@@ -41,7 +43,13 @@ $max = 1000000; // Vous pouvez définir la valeur maximale que vous souhaitez
 $randomNumber = mt_rand($min, $max);
 
 $ID_address=$randomNumber;
-$ID_Offre=$randomNumber;
+$ID_job=$randomNumber;
+
+//Vérifier si l'entreprise existe
+$stmt = $conn->prepare("SELECT ID_Entreprise FROM entreprise WHERE nom LIKE CONCAT('%', ?, '%')");
+$stmt->bind_param("s", $enterprise);
+$stmt->execute();
+$result = $stmt->get_result();
 
 // Préparer la requête SQL pour ajouter l'entreprise et son adresse
 // Première requête SQL
@@ -57,22 +65,47 @@ if ($stmt1->execute()) {
 $stmt1->close();
 
 // Deuxième requête SQL
-$sql2 = "INSERT INTO offre (ID_offre, Duree_Offre, Remuneration_Offre, Date_Offre, Nbr_Places_Offre, ID_Entreprise) 
-VALUES (?, ?, ?, ?, ?, ?)";
-$stmt2 = $conn->prepare($sql2);
-$stmt2->bind_param('isi', $ID_enterprise, $nom_enterprise, $ID_address);
+$stmt = $conn->prepare("SELECT ID_Entreprise FROM entreprise WHERE nom LIKE CONCAT('%', ?, '%')");
+$stmt->bind_param("s", $enterprise);
+$stmt->execute();
+$result = $stmt->get_result();
 
-if ($stmt2->execute()) {
-    echo "Entreprise ajoutée avec succès.";
+if ($result->num_rows > 0) {
+    // L'entreprise existe déjà, récupérer son ID
+    $row = $result->fetch_assoc();
+    $ID_enterprise = $row['ID_entreprise'];
 } else {
-    echo "Une erreur est survenue lors de l'ajout de l'entreprise : " . $conn->error;
+    // L'entreprise n'existe pas, insérer une nouvelle entreprise et récupérer son ID
+    echo "Une erreur est survenue lors de la récupération de l'ID de l'entreprise : " . $conn->error;
 }
-$stmt2->close();
+
+
+if ($result->num_rows > 0) {
+    // L'entreprise existe déjà, récupérer son ID
+    $row = $result->fetch_assoc();
+    $ID_enterprise = $row['ID_entreprise'];
+
+    // Préparer la requête SQL pour ajouter l'offre
+    $sql2 = "INSERT INTO offre (ID_offre, Duree_Offre, Remuneration_Offre, Date_Offre, Nbr_Places_Offre, ID_Entreprise) 
+    VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt2 = $conn->prepare($sql2);
+    $stmt2->bind_param('ididii', $ID_job, $period, $salary, $datePublication, $seats, $ID_enterprise);
+
+    if ($stmt2->execute()) {
+        echo "Offre ajoutée avec succès.";
+    } else {
+        echo "Une erreur est survenue lors de l'ajout de l'offre : " . $conn->error;
+    }
+    $stmt2->close();
+} else {
+    // L'entreprise n'existe pas, afficher un message d'erreur
+    echo "L'entreprise n'existe pas dans la base de données.";
+}
 
 // troisième requête SQL
-$sql3 = "INSERT INTO evaluer (ID_Entreprise, ID_Utilisateur, Evaluation) VALUES (?, ?, ?)";
+$sql3 = "INSERT INTO connaitre (ID_competences, ID_offre) VALUES (?, ?)";
 $stmt3 = $conn->prepare($sql3);
-$stmt3->bind_param('iii', $ID_enterprise, $_SESSION['user_id'], $trustPilot);
+$stmt3->bind_param('iii', $ID_skill, $ID_job);
 
 if ($stmt3->execute()) {
     echo "Evalutation ajouté avec succès.";
@@ -81,28 +114,11 @@ if ($stmt3->execute()) {
 }
 $stmt3->close();
 
-//Secteur d'activité (semblable à compétence), comparé pour voir si la competence existe déjà ou non
-$stmt = $conn->prepare("SELECT ID_Competence, Competence FROM competence WHERE Competence LIKE CONCAT('%', ?, '%')");
-$stmt->bind_param("s", $secteur);
-$stmt->execute();
-$result = $stmt->get_result();
 
-if ($result->num_rows > 0) {
-    // La compétence existe déjà, récupérer son ID
-    $row = $result->fetch_assoc();
-    $ID_Competence = $row['ID_Competence'];
-} else {
-    // La compétence n'existe pas, insérer une nouvelle compétence et récupérer son ID
-    $stmt = $conn->prepare("INSERT INTO competence (Competence) VALUES (?)");
-    $stmt->bind_param("s", $secteur);
-    $stmt->execute();
-    $ID_Competence = $stmt->insert_id;
-}
 
 // Mettre à jour l'entreprise avec l'ID de la compétence
-$stmt = $conn->prepare("UPDATE entreprise SET ID_Competence = ? WHERE ID_Entreprise = ?");
-$stmt->bind_param("ii", $ID_Competence, $ID_entreprise);
+$stmt = $conn->prepare("UPDATE connaitre SET ID_Competence = ? WHERE ID_offre = ?");
+$stmt->bind_param("ii", $ID_skill, $ID_job);
 $stmt->execute();
 
 ?>
-
