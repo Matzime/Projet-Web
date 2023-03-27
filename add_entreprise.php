035da6131662fcab379b5d47a8ad312b2a77af4e
HugoM1711 <116550014+HugoM1711@users.noreply.github.com>
2023-03-27 14:24:35 +0200

    dfghjk

--- add_entreprise.php
+++ add_entreprise.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'data_base_connexion.php';
+session_start();
 
 // Récupérer les données du formulaire d'inscription
 $nom_entreprise = $_POST['nom_entreprise'];
@@ -52,30 +53,6 @@ if ($stmt1->execute()) {
 }
 $stmt1->close();
 
-// Deuxième requête SQL
-$sql2 = "INSERT INTO entreprise (ID_Entreprise, Nom, ID_Competence, ID_Adresse) VALUES (?, ?, ?, ?)";
-$stmt2 = $conn->prepare($sql2);
-$stmt2->bind_param('isi', $ID_entreprise, $nom_entreprise, $ID_adresse);
-
-if ($stmt2->execute()) {
-    echo "Entreprise ajoutée avec succès.";
-} else {
-    echo "Une erreur est survenue lors de l'ajout de l'entreprise : " . $conn->error;
-}
-$stmt2->close();
-
-// troisième requête SQL
-$sql3 = "INSERT INTO evaluer (ID_Entreprise, ID_Utilisateur, Evaluation) VALUES (?, ?, ?)";
-$stmt3 = $conn->prepare($sql3);
-$stmt3->bind_param('iii', $ID_entreprise, $_SESSION['user_id'], $confiance_pilote);
-
-if ($stmt3->execute()) {
-    echo "Evalutation ajouté avec succès.";
-} else {
-    echo "Une erreur est survenue lors de l'ajout de l'évaluation : " . $conn->error;
-}
-$stmt3->close();
-
 //Secteur d'activité (semblable à compétence), comparé pour voir si la competence existe déjà ou non
 $stmt = $conn->prepare("SELECT ID_Competence, Competence FROM competence WHERE Competence LIKE CONCAT('%', ?, '%')");
 $stmt->bind_param("s", $secteur);
@@ -94,10 +71,30 @@ if ($result->num_rows > 0) {
     $ID_Competence = $stmt->insert_id;
 }
 
-// Mettre à jour l'entreprise avec l'ID de la compétence
-$stmt = $conn->prepare("UPDATE entreprise SET ID_Competence = ? WHERE ID_Entreprise = ?");
-$stmt->bind_param("ii", $ID_Competence, $ID_entreprise);
-$stmt->execute();
+// Insertion  de l'entreprise
+$sql2 = "INSERT INTO entreprise (ID_Entreprise, Nom, ID_Competence, ID_Adresse) VALUES (?, ?, ?, ?)";
+$stmt2 = $conn->prepare($sql2);
+$stmt2->bind_param('isii', $ID_entreprise, $nom_entreprise, $ID_Competence, $ID_adresse);
 
+if ($stmt2->execute()) {
+    echo "Entreprise ajoutée avec succès.";
+} else {
+    echo "Une erreur est survenue lors de l'ajout de l'entreprise : " . $conn->error;
+}
+$stmt2->close();
+
+
+
+// insertion evaluation
+$sql3 = "INSERT INTO evaluer (ID_Entreprise, ID_Utilisateur, Evaluation) VALUES (?, ?, ?)";
+$stmt3 = $conn->prepare($sql3);
+$stmt3->bind_param('iii', $ID_entreprise, $_COOKIE['user_id'], $confiance_pilote);
+
+if ($stmt3->execute()) {
+    echo "Evalutation ajouté avec succès.";
+} else {
+    echo "Une erreur est survenue lors de l'ajout de l'évaluation : " . $conn->error;
+}
+$stmt3->close();
 ?>
 
