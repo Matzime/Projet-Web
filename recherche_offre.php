c46bb69051f0b081e1ae42f109a200e802317bdb
HugoM1711 <116550014+HugoM1711@users.noreply.github.com>
2023-03-28 14:59:41 +0200

    final updates Offree search

--- recherche_offre.php
+++ recherche_offre.php
@@ -5,29 +5,30 @@ $competence = $_POST['competence'];
 $entreprise = $_POST['entreprise'];
 $duree = $_POST['duree'];
 $ville = $_POST['ville'];
-$remuneration= $_POST['remuneration'];
 $publication = $_POST['publication'];
-$nbrdeplaces = $_POST['nbrdeplaces'];
+
 
 if($duree==1){$dureeBis='< 8';}
-if($duree==2){$dureeBis='> 8 AND < 16';}
-if($duree==3){$dureeBis='> 16 AND < 24';}
+if($duree==2){$dureeBis='BETWEEN 8 AND 16';}
+if($duree==3){$dureeBis='BETWEEN 16 AND 24';}
 if($duree==4){$dureeBis='> 24';}
 
+if($publication){
+    $publicationBis='ORDER BY Date_Offre';}
+    else{$publicationBis='ORDER BY Date_Offre DESC';}
+
 $sql = "SELECT *
-        FROM entreprise
-        JOIN adresse ON entreprise.ID_Adresse = adresse.ID_Adresse
-        JOIN offre ON offre.ID_Entreprise = entreprise.ID_Entreprise
-        JOIN connaitre ON connaitre.ID_Offre = offre.ID_Offre
-        JOIN competence ON connaitre.ID_Competence = competence.ID_Competence
-        WHERE
-        (entreprise.Nom LIKE '%$entreprise%' OR '$entreprise' = '') AND
-        (competence.Competence = '$competence' OR '$competence' = '') AND
-        (Duree_offre .'$duree') AND
-        (Nom_Ville = '$ville' OR '$ville' = '') AND
-        (Date_Offre = '$publication' OR '$publication' = '') AND
-        (base_de_remuneration = '$remuneration' OR '$remuneration' = '') AND
-        (Nbr_Places_Offre = '$nbrdeplaces' OR '$nbrdeplaces' = '')";
+FROM entreprise
+JOIN adresse ON entreprise.ID_Adresse = adresse.ID_Adresse
+JOIN offre ON offre.ID_Entreprise = entreprise.ID_Entreprise
+JOIN connaitre ON connaitre.ID_Offre = offre.ID_Offre
+JOIN competence ON connaitre.ID_Competence = competence.ID_Competence
+WHERE
+(entreprise.Nom LIKE '%$entreprise%' OR '$entreprise' = '') AND
+(competence.Competence = '$competence' OR '$competence' = '') AND
+(Duree_offre $dureeBis) AND
+(Nom_Ville = '$ville' OR '$ville' = '') AND
+(Date_Offre = '$publication' OR '$publication' = '') $publicationBis";
 
 $result = $conn->query($sql);
 
@@ -39,7 +40,7 @@ if ($result->num_rows > 0) {
         echo "Ville : " . $row['Nom_Ville'] . "<br>";
         echo "Date de publication : " . $row['Date_Offre'] . "<br>";
         echo "Nombre de places : " . $row['Nbr_Places_Offre'] . "<br>";
-        echo "Base de rémunération : " . $row['base_de_remuneration'] . "<br>";
+        echo "Base de rémunération : " . $row['Remuneration_Offre'] . "<br>";
         echo "Durée : " . $row['Duree_offre'] . "<br><br>";
     }
 } else {
