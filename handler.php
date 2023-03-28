ad0a6ab07390bb281abe1844043f6229050d9315
HugoM1711 <116550014+HugoM1711@users.noreply.github.com>
2023-03-28 22:08:36 +0200

    marc demarco en susu

---
+++ handler.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+    
+    if ($action == 'rechercher') {
+        // Implémentez la logique de recherche ici
+    } elseif ($action == 'entreprise') {
+        // Implémentez la logique pour ajouter une entreprise ici
+    } elseif ($action == 'compte') {
+        // Implémentez la logique pour ajouter un compte ici
+    } elseif ($action == 'offre') {
+        // Implémentez la logique pour ajouter une offre ici
+    }
+}
+?>
